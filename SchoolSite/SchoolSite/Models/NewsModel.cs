e45aecdb648f112656a3d03f4e04124b39db3141
distantmoon <distantmoon@126.com>
2014-12-31 14:37:35 +0800

    Add News

---
+++ SchoolSite/SchoolSite/Models/NewsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace SchoolSite.Models
+{
+    public class NewsModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public virtual WebImage Images { get; set; }
+    }
+}
